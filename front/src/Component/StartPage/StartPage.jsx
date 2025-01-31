a48905bf751b4c4a435c747fbc952c8daf55d081
Emir <emir.ulusoy@student.forsbergsskola.se>
2025-01-31 14:38:09 +0100

    message box is written, cant do anyhting with it yet

---
+++ front/src/Component/StartPage/StartPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './StartPage.css'
+
+
+export default function StartPage() {
+  return (
+    <div className='cont'>
+        <div className='textDiv'>
+            <input type='text' placeholder='Write message'></input>
+        </div>
+        
+    </div>
+  )
+}
