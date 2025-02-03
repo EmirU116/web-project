4331f1c44580ff0099ba3900ac84c16e7a576aab
Emir <emir.ulusoy@student.forsbergsskola.se>
2025-02-03 08:44:18 +0100

    working on messaging box system, typing and pressing send button, will look like sending messgae but does nothing

--- front/src/Component/StartPage/StartPage.jsx
+++ front/src/Component/StartPage/StartPage.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './StartPage.css'
 
 
 export default function StartPage() {
+  const [inputmessage, setinputmessage] = useState("");
+
+  function  messageSender(e) {
+    setinputmessage(e.target.value);
+    console.log(inputmessage);
+
+    
+  }
+
+
   return (
         <div>
-                <div className='textDiv'>
-                    <div className='chat'>tset</div>
-                    <input type='text' placeholder='Write message'></input>
-                </div>
+            <div className='textDiv'>
+                <div className='chat'>tset</div>
+                <input type='text' placeholder='Write message' onChange={messageSender} value={inputmessage}></input>
+                <button onClick={messageSender}>SEND </button>
+            </div>
 
     </div>
   )
