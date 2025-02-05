63f2e2faedc9cece88d239e1d7f0f391fcf5a5be
Emir <emir.ulusoy@student.forsbergsskola.se>
2025-02-05 10:10:31 +0100

    simple setup for user creatino

--- front/src/Component/profile/create/createUser.jsx
+++ front/src/Component/profile/create/createUser.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function createUser() {
   return (
-    <div>
-        <h1>Create User</h1>
+    <div className="signupContainer">
+        <div className='signupFrom'>
+            <h1>Sign Up </h1>
+            <div className='UsernameContainer'>
+                <label>Username:</label>
+                <input type='text' placeholder='Enter Username' />
+            </div>
+            <button>Create</button>
+        </div>
     </div>
   );
 };
