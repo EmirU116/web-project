cda4532a4f1e9e2bd5218d3aa47011de32e126e5
Emir <emir.ulusoy@student.forsbergsskola.se>
2025-02-05 10:25:39 +0100

    setup for storing username, althought its not storing anywhere yet

--- front/src/Component/profile/create/createUser.jsx
+++ front/src/Component/profile/create/createUser.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function createUser() {
-  return (
-    <div className="signupContainer">
-        <div className='signupFrom'>
-            <h1>Sign Up </h1>
-            <div className='UsernameContainer'>
-                <label>Username:</label>
-                <input type='text' placeholder='Enter Username' />
+
+export default function CreateUser() {
+
+    const [username, setUsername] = useState("");
+
+    // function for updating the username input
+    const handleInput = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const UserCreation = () => {
+        alert('User Created');
+        setUsername('');
+    };
+
+
+    return (
+        <div className="signupContainer">
+            <div className='signupFrom'>
+                <h1>Sign Up </h1>
+                <div className='UsernameContainer'>
+                    <label>Username:</label>
+                    <input type='text' placeholder='Enter Username' value={username} onChange={handleInput} />
+                </div>
+                <button onClick={UserCreation}>Create</button>
             </div>
-            <button>Create</button>
         </div>
-    </div>
-  );
+    );
 };
