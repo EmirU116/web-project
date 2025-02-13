d111126699590da415469da47983aa9b5a225cf1
Emir <emir.ulusoy@student.forsbergsskola.se>
2025-02-13 15:45:30 +0100

    issues with create chat rooms

--- front/src/Component/StartPage/StartPage.jsx
+++ front/src/Component/StartPage/StartPage.jsx
@@ -20,6 +20,50 @@ export default function StartPage() {
   // User Status 
   const [userActivity, setuserActivity] = useState("online");
 
+  // display chat rooms
+  const [chatRooms, setChatRooms] = useState([]);
+
+
+  const createChats = async () => {
+    try{
+      const response = await fetch('http://localhost:4000/create_server', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: 'Chat Room',
+          owner: username
+        })
+      });
+      const data = await response.json();
+      if(response.ok){
+        setChatRooms(data);
+      } else {
+        console.log('Error: ', response.statusText);
+      }
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  };
+
+  useEffect(() => {
+    const displayChatRooms = async () => {
+      try{
+        const response = await fetch('http://localhost:4000/create_server');
+        const data = await response.json();
+        if(response.ok){
+          setChatRooms(data);
+        } else {
+          console.log('Error: ', response.statusText);
+        }
+      } catch (error) {
+        console.log('Error: ', error);
+      }
+
+    };
+    displayChatRooms();
+  }, []);
   // useEffect hook to run once when the component mounts
   useEffect(() => {
     
@@ -51,10 +95,10 @@ export default function StartPage() {
       setuserActivity('online');
       clearTimeout(inactiveTimer);
       inactiveTimer = setTimeout(() => 
-        setuserActivity('away'), 5000);
+        setuserActivity('away'), 10000);
     };
 
-    let inactiveTimer = setTimeout(() => setuserActivity('away'), 5000);
+    let inactiveTimer = setTimeout(() => setuserActivity('away'), 10000);
 
     window.addEventListener('mousemove', handleUserStatus);
     window.addEventListener('keydown', handleUserStatus);
@@ -70,6 +114,8 @@ export default function StartPage() {
   function messageSender(e) {
     setInputMessage(e.target.value);
   }
+
+
 
   // Function to send a message
   function sendMessage() {
@@ -96,6 +142,8 @@ export default function StartPage() {
     }    
   }
 
+
+
   // Render the component
   return (
     <div className='grid-container'>
@@ -106,6 +154,8 @@ export default function StartPage() {
         <li><button>User</button></li>
         <li><button>User</button></li>
         <li><button>User</button></li>
+
+        <button onClick={createChats}>Create Chat</button>
       </div>
       <div className='item2'>
           <div className='chat'>
@@ -128,7 +178,20 @@ export default function StartPage() {
         <Avatar name={username} size='70' round={true} />
         <li className={`userStatus ${userActivity}`}>{userActivity}</li>
       </div>
-      <div className='item4'>4</div>
+      <div className='item4'>
+          <p>Chat Rooms</p>
+          <ul>
+            {chatRooms.length === 0 ? (
+             <li>No servers</li> 
+            ) : (
+              chatRooms.map((server) => (
+                <li key={server.id}>
+                  {server.name} : (Owner: {server.owner})
+                </li>
+              ))
+            )}
+          </ul>
+      </div>
       
     </div>
   );
